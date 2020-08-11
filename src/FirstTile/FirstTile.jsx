69a6339917c7c9b4a446f11d2e36193edea3f3a3
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-08-11 22:35:16 +0500

    Finished Nav

---
+++ src/FirstTile/FirstTile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './FirstTile.css';   
+
+const FirstTile = () => {
+    return (
+        <div className="container-fluid">
+            <h1 className="heading-one">
+                Shoes and Sneakers
+            </h1>
+            <p className="para-one">Of the finest quality.</p>
+            <img className="rock-image" src="https://brobible.files.wordpress.com/2018/03/project-rock-under-armour-collection-new1.jpg" />
+        </div>
+    )
+}
+
+export default FirstTile
